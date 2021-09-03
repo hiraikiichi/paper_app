359d8a5add6903b834edcd2d84c1c0a398f58f9c
hiraikiichi <m1m21329@st.oit.ac.jp>
2021-09-03 20:58:23 +0900

    Footer調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,21 @@
-import '../styles/Footer.css';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const Footer = () => {
-    return (
-        <div className='site-footer'>
-            <p className = 'copyright'>@2021 KiichiHirai</p>
-        </div>
-    );
+  return (
+    <div className="page-footer" style={{ backgroundColor: "rgb(33, 37, 41)" }}>
+      <Container className="pt-3 pb-3">
+        <Row>
+          <Col className="text-center mt-1 mb-1">
+            <span className="align-middle ml-2" style={{color: "white"}}>
+                @2021 KiichiHirai
+            </span>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
