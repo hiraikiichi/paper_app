7bc0ce6d104627743382f1a2884c036fc6c0808f
hiraikiichi <m1m21329@st.oit.ac.jp>
2021-10-08 11:48:44 +0900

    add contact responsiv

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+import '../styles/Contactpage.css';
 
 const ContactPage = () => {
   return (
     <div style={{textAlign:"center"}}>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe8fUuNu9KqFbCrIOZloW5UhdJ10VQBqdsgfw5opERhtZI2aw/viewform?embedded=true" width="640" height="677" frameBorder="0" >
+      <iframe className="formFrame" src="https://docs.google.com/forms/d/e/1FAIpQLSe8fUuNu9KqFbCrIOZloW5UhdJ10VQBqdsgfw5opERhtZI2aw/viewform?embedded=true"  frameBorder="0" >
         読み込んでいます…
       </iframe>
     </div>
